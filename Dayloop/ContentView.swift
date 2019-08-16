bb8f2e5e121955b0fb595bde9f1a598608df45f1
Joshua <satwa.fr@gmail.com>
2019-08-16 13:56:31 +0200

    Initial Commit

---
+++ Dayloop/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Dayloop
+//
+//  Created by Joshua Tabakhoff on 16/08/2019.
+//  Copyright © 2019 Joshua Tabakhoff. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
